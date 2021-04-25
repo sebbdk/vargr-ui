6c9affd94e04ab97ee656b2faf3727bf35277d0e
sebbdk <sebb@sebb.dk>
2021-04-25 21:22:47 +0200

    added color scheme preview

---
+++ src/stories/atoms/color/color.stories.js
@@ -0,0 +1,19 @@
+import { ColorPreview } from './color';
+import { html } from 'htm/preact';
+import { CenterPreview } from '../../templates/centerpreview';
+
+export default {
+  title: 'Atoms/Color',
+  component: ColorPreview,
+  argTypes: {},
+};
+
+const Template = (args) => html`<${CenterPreview}><${ColorPreview} {...args} /></${CenterPreview}>`;
+
+export const PrimaryColor = Template.bind({});
+PrimaryColor.args = {
+    primary: 'red',
+    secondary: 'blue',
+    contrast: 'black',
+    background: 'white'
+};
