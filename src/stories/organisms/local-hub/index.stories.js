54576a9a5332e2ead8d0cc5149b9dc6e67993172
sebbdk <sebb@sebb.dk>
2021-04-25 21:23:44 +0200

    use center preview for localhub

--- src/stories/organisms/local-hub/index.stories.js
+++ src/stories/organisms/local-hub/index.stories.js
@@ -1,6 +1,6 @@
-/** @jsx h */
-import { h } from 'preact';
+import { html } from 'htm/preact';
 import { LocalHub } from './index';
+import { CenterPreview } from '../../templates/centerpreview/index';
 
 export default {
   title: 'Organisms/Hub',
@@ -11,7 +11,7 @@ export default {
   },
 };
 
-const Template = (args) => <LocalHub {...args} />;
+const Template = (args) => html`<${CenterPreview}><${LocalHub} ...${args} /></${CenterPreview}>`;
 
 export const RegularHub = Template.bind({});
 RegularHub.args = {};
