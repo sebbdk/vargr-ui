00d8024c0c7c8bf1103641f13ca6d9ab88f94730
sebbdk <sebb@sebb.dk>
2021-04-25 22:24:50 +0200

    add public domain images for examples

--- src/stories/organisms/gallery-index/index.stories.js
+++ src/stories/organisms/gallery-index/index.stories.js
@@ -13,56 +13,25 @@ export default {
 
 const Template = (args) => <Gallery {...args} />;
 
+const demoPages = (new Array(36)).fill(undefined).map((i, index) => require(`stories/assets/atomic_war/${index}.jpg`));
+
 export const NoneInteractive = Template.bind({});
 NoneInteractive.args = {
   title: 'And they talked',
-  images: [
-    require('./demo_files/663875809.jpg'),
-    require('./demo_files/664934143.jpg'),
-    require('./demo_files/666026594.jpeg'),
-    require('./demo_files/668657374.jpg'),
-    require('./demo_files/668763917.jpg'),
-    require('./demo_files/671049702.jpg'),
-    require('./demo_files/671792291.png'),
-    require('./demo_files/675709990.jpeg'),
-    require('./demo_files/677267678.jpeg'),
-    require('./demo_files/680186389.png'),
-    require('./demo_files/681183447.jpg'),
-    require('./demo_files/685368844.jpg'),
-    require('./demo_files/694404725.jpg')
-  ],
+  images: demoPages,
   onPick: undefined
 };
 
 export const ActionsDemo = Template.bind({});
 ActionsDemo.args = {
   title: 'And they talked',
-  images: [
-    require('./demo_files/663875809.jpg'),
-    require('./demo_files/664934143.jpg'),
-    require('./demo_files/666026594.jpeg'),
-    require('./demo_files/668657374.jpg'),
-    require('./demo_files/668763917.jpg'),
-    require('./demo_files/671049702.jpg'),
-    require('./demo_files/671792291.png'),
-    require('./demo_files/675709990.jpeg'),
-    require('./demo_files/677267678.jpeg'),
-    require('./demo_files/680186389.png'),
-    require('./demo_files/681183447.jpg'),
-    require('./demo_files/685368844.jpg'),
-    require('./demo_files/694404725.jpg')
-  ]
+  images: demoPages
 };
 
 export const WithPickAction = Template.bind({});
 WithPickAction.args = {
   title: 'Brave new world',
-  images: [
-    require('./demo_files/680186389.png'),
-    require('./demo_files/681183447.jpg'),
-    require('./demo_files/685368844.jpg'),
-    require('./demo_files/694404725.jpg')
-  ],
+  images: demoPages,
   onPick: (index, images) => {
     window.open(images[index],'_blank');
   }
--- src/stories/organisms/gallery-reader/index.stories.js
+++ src/stories/organisms/gallery-reader/index.stories.js
@@ -21,40 +21,15 @@ const FakePage = styled.div`
 `;
 
 const Template = (args) => <FakePage><GalleryReader {...args} /></FakePage>;
+const demoPages = (new Array(36)).fill(undefined).map((i, index) => require(`stories/assets/atomic_war/${index}.jpg`));
 
 export const SinglePage = Template.bind({});
 SinglePage.args = {
-  images: [
-    require('./demo_files/663875809.jpg'),
-    require('./demo_files/664934143.jpg'),
-    require('./demo_files/666026594.jpeg'),
-    require('./demo_files/668657374.jpg'),
-    require('./demo_files/668763917.jpg'),
-    require('./demo_files/671049702.jpg'),
-    require('./demo_files/671792291.png'),
-    require('./demo_files/675709990.jpeg'),
-    require('./demo_files/677267678.jpeg'),
-    require('./demo_files/680186389.png'),
-    require('./demo_files/681183447.jpg'),
-    require('./demo_files/685368844.jpg'),
-    require('./demo_files/694404725.jpg')
-  ]
+  images: demoPages
 };
 
 export const DualPage = Template.bind({});
 DualPage.args = {
-  images: [
-    require('./demo_files/680186389.png'),
-    require('./demo_files/681183447.jpg'),
-    require('./demo_files/685368844.jpg'),
-    require('./demo_files/694404725.jpg'),
-    require('./demo_files/671792291.png'),
-    require('./demo_files/675709990.jpeg'),
-    require('./demo_files/677267678.jpeg'),
-    require('./demo_files/680186389.png'),
-    require('./demo_files/681183447.jpg'),
-    require('./demo_files/685368844.jpg'),
-    require('./demo_files/694404725.jpg')
-  ],
+  images: demoPages,
   dualPage: true
 };
