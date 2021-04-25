196ee6006c6a26157289db88e671e8ef7d3ecd1b
sebbdk <sebb@sebb.dk>
2021-04-25 21:22:24 +0200

    added center preview template

---
+++ src/stories/templates/centerpreview/centerpreview.stories.js
@@ -0,0 +1,13 @@
+import { CenterPreview } from './index.js';
+import { html } from 'htm/preact';
+
+export default {
+  title: 'templates/centerpreview',
+  component: CenterPreview,
+  argTypes: {},
+};
+
+const Template = (args, children) => html`<${CenterPreview}>
+    <div style="background-color: #BBB; padding: 0.5rem;">Hi there!</div>
+</${CenterPreview}>`;
+export const primary= Template.bind({});
---
+++ src/stories/templates/centerpreview/index.js
@@ -0,0 +1,19 @@
+import { html } from "htm/preact"
+import styled from "styled-components"
+
+const CenterpreviewElm = styled.div`
+    padding: 0.5rem;
+    max-width: 600px;
+    min-width: 80vh;
+    min-height: 90vh;
+    display: flex;
+    margin: auto;
+    padding: 1rem;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CenterPreview = (props) => {
+    return html`<${CenterpreviewElm}>${props.children}</${CenterpreviewElm}>`
+}
