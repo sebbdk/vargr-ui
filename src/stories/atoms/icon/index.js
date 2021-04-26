805dcf07d794cfc064e1c91690cbcad45fd90376
Kasper Sebb' brandt <sebb@sebb.dk>
2021-04-26 22:53:08 +0200

    added icon atom

---
+++ src/stories/atoms/icon/index.js
@@ -0,0 +1,26 @@
+import { html } from "htm/preact"
+import styled from "styled-components"
+
+const IconElm = styled.div`
+    font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px;
+    font-display: block;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    direction: ltr;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    -moz-osx-font-smoothing: grayscale;
+    font-feature-settings: 'liga';
+
+    vertical-align: middle;
+    display: inline-block;
+`;
+
+export const Icon = (props) => {
+    return html`<${IconElm}>${props.children}</${IconElm}>`
+}
---
+++ src/stories/atoms/icon/index.stories.js
@@ -0,0 +1,24 @@
+import { Icon } from './index.js';
+import { html } from 'htm/preact';
+import { BasePage } from 'stories/templates/basepage';
+import { CenterPreview } from 'stories/templates/centerpreview';
+
+export default {
+  title: 'atoms/Icon',
+  component: Icon,
+  argTypes: {},
+};
+
+const Template = (args) => html`<${BasePage}>
+    <${CenterPreview}>
+      <${Icon}>menu</${Icon}>
+      <${Icon}>close</${Icon}>
+      <${Icon}>favorite</${Icon}>
+      <${Icon}>loyalty</${Icon}>
+      <${Icon}>delete</${Icon}>
+    </${CenterPreview}>
+</${BasePage}>`;
+
+export const primary= Template.bind({});
+    primary.args = {
+};
