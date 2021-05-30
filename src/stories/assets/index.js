68fc2a560cd4194b39d72ce1cc5780b1a0e826eb
sebb <sebbdk@users.noreply.github.com>
2021-05-30 23:23:18 +0200

    use card for gallery

--- src/stories/assets/index.js
+++ src/stories/assets/index.js
@@ -1,4 +1,11 @@
 const folder = process.env.ASSETS_OVERRIDE || 'atomic_war';
 
-export const DemoPages1 = (new Array(22)).fill(undefined).map((i, index) => require(`stories/assets/${folder}/1/${index+1}.jpg`));
-export const DemoPages2 = (new Array(20)).fill(undefined).map((i, index) => require(`stories/assets/${folder}/2/${index+1}.jpg`));
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
+
+//export const DemoPages1 = (new Array(22)).fill(undefined).map((i, index) => require(`stories/assets/${folder}/1/${index+1}.jpg`));
+export const DemoPages1 = (new Array(201)).fill(undefined).map((i, index) => require(`stories/assets/local_examples/maidens_of_steel/${pad(index+1, 3)}.png`));
+export const DemoPages2 = DemoPages1;//(new Array(20)).fill(undefined).map((i, index) => require(`stories/assets/${folder}/2/${index+1}.jpg`));
