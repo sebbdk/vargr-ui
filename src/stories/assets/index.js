bb0a9f71a1bf9d718e60c57055d31a9725f6bc28
sebbdk <sebbdk@users.noreply.github.com>
2021-05-31 19:13:56 +0200

    fixed assets refs

--- src/stories/assets/index.js
+++ src/stories/assets/index.js
@@ -6,6 +6,6 @@ function pad(n, width, z) {
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
   }
 
-//export const DemoPages1 = (new Array(22)).fill(undefined).map((i, index) => require(`stories/assets/${folder}/1/${index+1}.jpg`));
-export const DemoPages1 = (new Array(201)).fill(undefined).map((i, index) => require(`stories/assets/local_examples/maidens_of_steel/${pad(index+1, 3)}.png`));
-export const DemoPages2 = DemoPages1;//(new Array(20)).fill(undefined).map((i, index) => require(`stories/assets/${folder}/2/${index+1}.jpg`));
+export const DemoPages1 = (new Array(22)).fill(undefined).map((i, index) => require(`stories/assets/atomic_war/1/${index+1}.jpg`));
+export const DemoPages2 = DemoPages1;
+export const DemoPages3 = DemoPages1;
