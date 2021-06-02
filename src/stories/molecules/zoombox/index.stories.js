c41234fb3df6b09ac204072440f0602921819dfc
sebbdk <sebbdk@users.noreply.github.com>
2021-06-02 23:03:18 +0200

    added scroll zoom to zoombox

---
+++ src/stories/molecules/zoombox/index.stories.js
@@ -0,0 +1,50 @@
+import { ZoomBox } from './index';
+import styled from 'styled-components';
+import { html } from 'htm/preact';
+
+export default {
+  title: 'Organisms/ZoomBox',
+  component: ZoomBox,
+  argTypes: {
+    images: [],
+    dualPage: false
+  }
+};
+
+const ImagePage = styled.img`
+    object-fit: cover;
+    object-position: center;
+    display: block;
+    height: 100%;
+    width: 100%;
+
+    ${({ isDualPage }) => isDualPage ? `
+        &:first-of-type {
+            object-position: top right;
+        }
+        
+        &:last-of-type {
+            object-position: top left;
+        }
+    ` : ''};
+`;
+
+const ParentContainer = styled.div`
+    width: 500px;
+    height: 500px;
+    border: 5px solid red;
+    position: relative;
+    background-color: #DDD;
+    margin: auto;
+`;
+
+const Template = (args) => html`
+    <${ParentContainer}>
+        <${ZoomBox} ...${args}>
+            <${ImagePage} src=${require(`stories/assets/resizeme_hori.jpg`)} />
+        </${ZoomBox}>
+    </${ParentContainer}>
+`;
+
+export const SinglePage = Template.bind({});
+SinglePage.args = {};
